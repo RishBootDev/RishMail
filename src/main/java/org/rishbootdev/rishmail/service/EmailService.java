f8f4b678a9a526760b265d5910c0cde4f8f36783
Rishabh <rishabhdubey1472004@gmail.com>
2025-11-18 08:28:10 +0530

    updated the logic for generating email response

--- src/main/java/org/rishbootdev/rishmail/service/EmailService.java
+++ src/main/java/org/rishbootdev/rishmail/service/EmailService.java
@@ -1,19 +1,49 @@
 package org.rishbootdev.rishmail.service;
 
 
-import lombok.RequiredArgsConstructor;
+import lombok.AllArgsConstructor;
 import org.rishbootdev.rishmail.dto.EmailRequest;
 import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.stereotype.Service;
 
 @Service
-@RequiredArgsConstructor
+@AllArgsConstructor
 public class EmailService {
 
     private final ChatClient chatClient;
 
     public String generateEmailReply(EmailRequest emailRequest) {
 
-        return "";
+        String prompt=buildPrompt(emailRequest);
+
+       String requestBody=String.format("""
+               {
+                 "contents": [
+                   {
+                     "parts": [
+                       {
+                         "text": "%s"
+                       }
+                     ]
+                   }
+                 ]
+               }
+               
+               """,prompt);
+
+          return " ";
+    }
+    public String buildPrompt(EmailRequest emailRequest){
+
+        StringBuilder prompt=new StringBuilder();
+        prompt.append("Generate a professional email reply for the following email:");
+        if(emailRequest.getTone() != null && !emailRequest.getTone().isEmpty()){
+            prompt.append("Use a ")
+                    .append(emailRequest.getTone())
+                    .append(" tone.");
+        }
+        prompt.append("Original Email: \n").append(emailRequest.getEmailContent());
+
+        return prompt.toString();
     }
 }
